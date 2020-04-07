3135011a5e6097e4c4d5cf05921ec95aebca2854
Spencer Purcell <spencerpurcell@Spencers-MacBook-Pro.local>
2020-04-07 16:28:21 -0700

    Toggle between project types

--- myportfolio/src/components/Projects.js
+++ myportfolio/src/components/Projects.js
@@ -1,10 +1,39 @@
 import React, { Component} from "react";
+import { Tabs, Tab} from "react-mdl";
 
 
 class Projects extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0};
+  }
+  toggleCategories() {
+  if(this.state.activeTab === 0) {
+    return (
+      <div><h1>React</h1></div>
+    )
+  } else if(this.state.activeTab ===1) {
+    return(
+      <div><h1>MongoDB</h1></div>
+    )
+  } else if(this.state.activeTab === 2) {
+    return(
+      <div><h1>Javascript</h1></div>
+    )
+  }
+}
   render() {
     return (
-    <div><h1>Projects</h1></div>
+    <div className="category-tabs">
+      <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+        <Tab>React</Tab>
+        <Tab>MongoDB</Tab>
+        <Tab>Javascript</Tab>
+      </Tabs>
+      <section className="projects-grid">
+        {this.toggleCategories()}
+      </section>
+    </div>
     )}
 }
 
