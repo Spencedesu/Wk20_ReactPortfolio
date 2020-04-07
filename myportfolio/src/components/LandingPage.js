87f305c6c541d200edaebb6beefc6b3e7909ecce
Spencer Purcell <spencerpurcell@Spencers-MacBook-Pro.local>
2020-04-07 16:09:32 -0700

    finished styling main page

--- myportfolio/src/components/LandingPage.js
+++ myportfolio/src/components/LandingPage.js
@@ -12,6 +12,22 @@ class LandingPage extends Component{
             className="bitmoji"
              src="https://sdk.bitmoji.com/render/panel/6ce76d26-9c7a-4fd2-8675-f5d4225363d6-3d139a7d-1957-466d-91a8-73b8eca8218b-v1.png?transparent=1&palette=1" alt="spencerBitmoji"
             />
+          <div className="banner-text">
+            <h1>Full Stack Web Developer</h1>
+            <hr/>
+            <p>HTML/CSS | Javascript | NodeJS | Express | MongoDB | React | </p>
+            <div className="social-links">
+            <a href="https://github.com/Spencedesu" rel="noopener noreferrer" target="_blank">
+              <i className="fa fa-github"  aria-hidden="true"/>
+            </a>
+              <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+              <i className="fa fa-linkedin"  aria-hidden="true"/>
+            </a>
+              <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+              <i className="fa fa-free-code-camp"  aria-hidden="true"/>
+            </a>
+            </div>
+          </div>
         </Cell>
 
       </Grid>
